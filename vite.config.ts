c88185f8a6c5f8c3066171b907b10df902ad67db
Shankar Mattaparthi <shankar.mattaparthi@supplier.io>
2026-06-19 11:43:45 +0530

    fix: add .gitignore, restore base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/',
+  base: command === 'build' ? '/explorer-design-system/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}))
